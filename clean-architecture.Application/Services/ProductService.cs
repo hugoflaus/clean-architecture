bc7d252de910e3a562c7056eba175dc8925fff41
Hugo Flausino <hugo.flausino@hotmail.com>
2021-02-25 07:50:13 -0300

    fix builder name constructor and parameter in the Add method

--- clean-architecture.Application/Services/ProductService.cs
+++ clean-architecture.Application/Services/ProductService.cs
@@ -1,15 +1,19 @@
+using clean_architecture.Application.InputModels;
+using clean_architecture.Application.ViewModels;
+using clean_architecture.Core.Interfaces.Repositories;
+
 namespace clean_architecture.Application.Services
 {
     public class ProductService
     {
         private readonly IProductRepository _productRepository;
 
-        public ProductViewModel(IProductRepository productRepository)
+        public ProductService(IProductRepository productRepository)
         {
             _productRepository = productRepository;
         }
 
-        public void Add(ProductModel productModel)
+        public void Add(ProductInputModel productModel)
         {
 
         }
